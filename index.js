b3b8daa21a5dbc9ccda204e3bb1af6593aab9ec1
momin-abdurraheman <abdurjmi1923@gmail.com>
2022-09-25 15:19:56 +0530

    Added New routes

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { response } = require("express");
 const express = require("express")
+// Imported users file from data so that we can access it
+const { users } = require('./data/users.json');
 
 const app = express();
 
@@ -12,6 +14,115 @@ app.get("/", (req, res) => {
         message: "Server is uptodate and running perfectly"
     });
 });
+
+/**
+ * Route : /user
+ * Method: GET
+ * Description : GET all user details
+ * Access : Public
+ * Parameters : None
+ */
+
+app.get("/users", (req, res) => {
+    res.status(200).json({
+        Success: true,
+        data: users
+    });
+});
+
+/**
+ * Route : /users/:id
+ * Method: GET 
+ * Description : GET a single user details by id
+ * Access : Public
+ * Parameters : id
+ */
+
+app.get("/users/:id", (req, res) => {
+    // Getting user id using parameters of API
+    const { id } = req.params;
+    const user = users.find((each) => each.id === id);
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "User not found",
+        });
+    }
+    return res.status(200).json({
+        success: true,
+        data: user,
+    });
+});
+
+/**
+ * Route : /users
+ * Method: POST
+ * Description : Create New User
+ * Access : Public
+ * Parameters : None
+ */
+
+app.post("/users", (req, res) => {
+    const { id, name, surname, email, subscriptionType, subscriptionDate } =
+        req.body;
+
+    const user = users.find((each) => each.id === id)
+
+
+    if (user) {
+        return res.status(404).json({
+            success: false,
+            message: "User already exist with this id",
+        })
+    }
+    users.push({
+        id,
+        name,
+        surname,
+        email,
+        subscriptionType,
+        subscriptionDate,
+    })
+
+    return res.status(201).json({
+        success: true,
+        data: users,
+
+    })
+});
+
+/**
+ * Route : /users/:id
+ * Method: PUT 
+ * Description : Updating User
+ * Access : Public
+ * Parameters : id
+ */
+app.put("/users/:id", (req, res) => {
+    const { id } = req.params;
+    const { data } = req.body;
+    const user = users.find((each) => each.id === id);
+    if (!user) {
+        return res.status(404).json({
+            success: false,
+            message: "User Not found",
+        })
+    }
+    const updatedUser = users.map((each) => {
+        if (each.id === id) {
+            return {
+                ...each,
+                ...data,
+            };
+        }
+        return each;
+    });
+    return res.status(201).json({
+        success: true,
+        data: updatedUser,
+    })
+
+})
 
 app.get("*", (req, res) => {
     response.status(404).json({
