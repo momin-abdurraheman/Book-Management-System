c67eacfc965185c549d1fe2cefa7f19d000685f5
momin-abdurraheman <abdurjmi1923@gmail.com>
2022-10-14 20:31:59 +0530

    Created connection with mongodb and added dotenv file and data

--- databaseConnection.js
+++ databaseConnection.js
@@ -3,6 +3,18 @@ const mongoose = require('mongoose');
 function Dbconnection() {
     const DB_URL = process.env.MONGO_URI;
 
+    mongoose.connect(DB_URL);
+
+    // Detail of the above connection is saved inside variable named Db
+    const db = mongoose.connection;
+
+    // On will be called the whole time while once will be called only once 
+    db.on("error", console.error.bind(console, "Connection error: "));
+    db.once('open', function () {
+        console.log("Database is connected ... ")
+
+    })
+
 
 };
 
